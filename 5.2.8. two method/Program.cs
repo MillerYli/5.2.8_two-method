320e207c8dc9e6607a940f1a6c98b5a943dbdf81
Yulia Miller <miller82.98@mail.ru>
2022-12-26 21:57:35 -0800

    Добавьте файлы проекта.

---
+++ 5.2.8. two method/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
+
+        static int[] SortArray(int[] result)
+        {
+            int number = 0;
+            for (int i = 0; i < result.Length; i++)
+                for (int j = i + 1; j < result.Length; j++)
+                    if (result[i] > result[j])
+                    {
+                        number = result[i];
+                        result[i] = result[j];
+                        result[j] = number;
+                    }
+            return result;
+
+
+
+            //for (int k = 0; k < result.Length; k++)
+            //{
+            //    Console.Write(result[k] + " ");
+            //}
+
+
+        }
+        public static void Main(string[] args)
+        {
+            int[] array = GetArrayFromConsole();
+
+            SortArray(array);
+            
+        }
+    }
+}
